bb3647b2ff6d46ab80e0093a4c248574b9cb8cba
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-05-18 17:26:58 -0300

    Delete 27-04 directory

--- 27-04/exercicio04.py
+++
@@ -1,9 +0,0 @@
-peoples = []
-for x in range (5):
-    peoples.append(input("insert your name: "))
-print()
-for person in range (5):
-    print(peoples[person], end=" ")
-print()
-for y in range (4,-1,-1):
-    print(peoples[y],end=" ")
