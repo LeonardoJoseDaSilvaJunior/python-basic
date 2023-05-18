7f95ee3b7d50d78074e13ea1cee321cb812dcbe5
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-05-18 17:23:30 -0300

    Delete 11-5 directory

--- 11-5/exercicio02.py
+++
@@ -1,3 +0,0 @@
-from library.functions import *
-valor = float(input("Insira um valor: "))
-print(positivo_Negativo(valor))
--- 11-5/exercicio04.py
+++
@@ -1,7 +0,0 @@
-from library.functions import  *
-n1 = int(input(""))
-n2 = int(input(""))
-n3 = int(input(""))
-n4 = int(input(""))
-
-print(infinita(n1,n2,n3,n4))
