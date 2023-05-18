0b57f269bec2bd907e4d82a2fed1aff3fb8c8e6a
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-05-18 17:26:33 -0300

    Delete 24-4 directory

--- 24-4/exercicio04.py
+++
@@ -1,7 +0,0 @@
-numbers = []
-reverse = []
-for x in range (20):
-    numbers.append(float(input("Insira numeros: ")))
-for x in range (20-1,0,-1):
-    reverse.append(numbers[x])
-print(reverse)
--- 24-4/exxercicio03.py
+++
@@ -1,8 +0,0 @@
-A = []
-M = []
-for x in range (10):
-    A.append(float(input("insira o numero: ")))
-x = float(input("Insira o multiplicador : "))
-for i in range(0,10):
-    M.append(A[i]*x)
-print(M,end=" ")
