e90d0b9c70266d73b8907d9d10f5cec8578dceb0
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-05-18 17:26:20 -0300

    Delete 19-4 directory

--- 19-4/exercicio02.py
+++
@@ -1,9 +0,0 @@
-listaNomes = []
-quantidade_alunos = int(input("insira a quantidade de alunos: "))
-
-for i in range (0,quantidade_alunos):
-    listaNomes.append(input("Insira o nome do alunos: "))
-print(" ")
-for x in range (quantidade_alunos):
-
-    print(listaNomes[x] ," Posição :" , x)
